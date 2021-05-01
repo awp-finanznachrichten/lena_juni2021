909bb50b58a8592eda064e898cc988cb6995022e
awp-finanznachrichten <sw@awp.ch>
2021-05-01 09:37:31 +0200

    commit from Rstudio

---
+++ config.R
@@ -0,0 +1,97 @@
+#Bibliotheken laden
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(readr)
+library(ggplot2)
+library(stringr)
+library(stringi)
+library(xml2)
+library(rjson)
+library(jsonlite)
+library(readxl)
+library(git2r)
+library(DatawRappr)
+
+cat("Benoetigte Bibliotheken geladen\n")
+
+#Link zu JSON-Daten / Daten einlesen
+link_json <- "https://app-prod-static-voteinfo.s3.eu-central-1.amazonaws.com/v1/ogd/sd-t-17-02-20210307-eidgAbstimmung.json" 
+json_data <- fromJSON(link_json, flatten = TRUE)
+
+link_json_kantone <- "https://app-prod-static-voteinfo.s3.eu-central-1.amazonaws.com/v1/ogd/sd-t-17-02-20210307-kantAbstimmung.json"
+json_data_kantone <- fromJSON(link_json_kantone, flatten = TRUE)
+
+cat("Aktuelle Abstimmungsdaten geladen\n")
+
+#Kurznamen Vorlagen (Verwendet im File mit den Textbausteinen)
+vorlagen_short <- c("Verhuellungsverbot","E-ID-Gesetz","Indonesien")
+
+###Kurznamen und Nummern kantonale Vorlagen
+kantonal_short <- c("ZH_Sozialdetektive","GE_Coronavirus")
+
+#Nummer in JSON 
+kantonal_number <- c(1,6) 
+
+#Falls mehrere Vorlagen innerhalb eines Kantons, Vorlage auswaehlen
+#kantonal_add <- c(2,1)
+
+###Kurznamen und Nummern kantonale Vorlagen Spezialfaelle
+#kantonal_short_special <- c("ZH_Polizeimeldungen","BE_Handel")
+
+#Nummer in JSON 
+#kantonal_number_special <- c(1,2) 
+
+#Falls mehrere Vorlagen innerhalb eines Kantons, Vorlage auswaehlen
+#kantonal_add_special <- c(3,1) 
+
+###Vorhandene Daten laden
+#daten_minarett_bfs <- read_excel("Data/daten_minarett_bfs.xls", 
+#                               skip = 10)
+
+#cat("Daten zu historischen Abstimmungen geladen\n")
+
+#Metadaten Gemeinden und Kantone
+meta_gmd_kt <- read_csv("Data/MASTERFILE_GDE_NEW.csv")
+
+cat("Metadaten zu Gemeinden und Kantonen geladen\n")
+
+#Datawrapper-Codes
+datawrapper_codes <- read_excel("Data/Datawrapper_Codes.xlsx")
+
+gitcommit <- function(msg = "commit from Rstudio", dir = getwd()){
+  cmd = sprintf("git commit -m\"%s\"",msg)
+  system(cmd)
+}
+
+gitstatus <- function(dir = getwd()){
+  cmd_list <- list(
+    cmd1 = tolower(substr(dir,1,2)),
+    cmd2 = paste("cd",dir),
+    cmd3 = "git status"
+  )
+  cmd <- paste(unlist(cmd_list),collapse = " & ")
+  shell(cmd)
+}
+
+gitadd <- function(dir = getwd()){
+  cmd_list <- list(
+    cmd1 = tolower(substr(dir,1,2)),
+    cmd2 = paste("cd",dir),
+    cmd3 = "git add --all"
+  )
+  cmd <- paste(unlist(cmd_list),collapse = " & ")
+  shell(cmd)
+}
+
+gitpush <- function(dir = getwd()){
+  cmd_list <- list(
+    cmd1 = tolower(substr(dir,1,2)),
+    cmd2 = paste("cd",dir),
+    cmd3 = "git push"
+  )
+  cmd <- paste(unlist(cmd_list),collapse = " & ")
+  shell(cmd)
+}
+
+
---
+++ nationale_abstimmungen.R
@@ -0,0 +1,272 @@
+for (i in 1:length(vorlagen_short)) {
+  
+  cat(paste0("\nErmittle Daten für folgende Vorlage: ",vorlagen$text[i],"\n"))
+  
+  
+  ###Nationale Resultate aus JSON auslesen
+  results_national <- get_results(json_data,i,level="national")
+
+  
+  ###Resultate aus JSON auslesen für Gemeinden
+  results <- get_results(json_data,i)
+
+#Simulation Gemeinden
+#source("data_simulation_gemeinden.R")
+  
+
+  #Emergency adapt
+  #results$gebietAusgezaehlt[155] <- TRUE
+  #results$gebietAusgezaehlt[897] <- TRUE
+  #results$gebietAusgezaehlt[898] <- TRUE
+  #results$gebietAusgezaehlt[899] <- TRUE
+  
+  #Daten anpassen Gemeinden
+  results <- treat_gemeinden(results)
+  results <- format_data_g(results)
+  
+  #Kantonsdaten hinzufügen
+  results_kantone <- get_results(json_data,i,"cantonal")
+  
+  #Simulation Kantone
+  #source("data_simulation_kantone.R")
+  
+  Ja_Stimmen_Kanton <- results_kantone %>%
+    select(Kantons_Nr,jaStimmenInProzent) %>%
+    rename(Ja_Stimmen_In_Prozent_Kanton = jaStimmenInProzent) %>%
+    mutate(Highest_Yes_Kant = FALSE,
+           Highest_No_Kant = FALSE)
+  
+  results <- merge(results,Ja_Stimmen_Kanton)
+  
+  
+  #Wie viele Gemeinden sind ausgezählt
+  cat(paste0(sum(results$Gebiet_Ausgezaehlt)," Gemeinden sind ausgezählt.\n"))
+  
+  #Neue Variablen
+  results <- results %>%
+    mutate(Ja_Nein = NA,
+           Oui_Non = NA,
+           Unentschieden = NA,
+           Einstimmig_Ja = NA,
+           Einstimmig_Nein = NA,
+           kleine_Gemeinde = NA,
+           Storyboard = NA,
+           Text_d = "Die Resultate dieser Gemeinde sind noch nicht bekannt.",
+           Text_f = "Les résultats ne sont pas encore connus dans cette commune.",
+           Text_i = "I resultati di questa comune non sono ancora noti.")
+  
+  hist_check <- FALSE
+  
+  #Ausgezählte Gemeinden auswählen
+  results_notavailable <- results[results$Gebiet_Ausgezaehlt == FALSE,]
+  results <- results[results$Gebiet_Ausgezaehlt == TRUE,]
+  
+  #Sind schon Daten vorhanden?
+  if (nrow(results) > 0) {
+    
+    #Daten anpassen
+    results <- augment_raw_data(results)
+    
+    #Intros generieren
+    results <- normal_intro(results)
+    
+    #LENA-Classics (falls alle Gemeinden ausgezählt):
+    if (nrow(results_notavailable) == 0) {
+      
+      results <- lena_classics(results)
+      
+    }  
+    
+    #Historischer Vergleich (falls vorhanden)
+    
+    #Check Vorlagen-ID
+    
+    if (vorlagen$id[i] == "6380") { 
+      
+      hist_check <- TRUE 
+      data_hist <- format_data_hist(daten_minarett_bfs)
+      results <- merge(results,data_hist,all.x = TRUE)
+      results <- hist_storyfinder(results)
+      
+    }
+    
+    
+    #Vergleich innerhalb des Kantons (falls alle Daten vom Kanton vorhanden)
+    
+    #Check Vorlagen-ID
+    if (vorlagen$id[i] == "6390" || vorlagen$id[i] == "6400") {
+      
+      #Falls mindestens ein Kanton ausgezählt -> Stories für die Kantone finden
+      
+      if (length(unique(results_notavailable$Kantons_Nr)) < 26) {
+        
+        results <- kanton_storyfinder(results)
+        
+      }
+      
+    }
+    
+    
+    ###Storybuilder
+    
+    #Textvorlagen laden
+    Textbausteine <- as.data.frame(read_excel("Data/Textbausteine_LENA_Maerz2021.xlsx", 
+                                              sheet = vorlagen_short[i]))
+    cat("Textvorlagen geladen\n\n")
+    
+    #Texte einfügen
+    results <- build_texts(results)
+    
+    #Variablen ersetzen 
+    results <- replace_variables(results)
+    
+    ###Texte anpassen und optimieren
+    results <- excuse_my_french(results)
+    
+    #Print out texts
+    #cat(paste0(results$Gemeinde_d,"\n",results$Text_d,"\n\n",results$Text_f,collapse="\n\n"))
+    
+  }
+  ###Ausgezählte und nicht ausgezählte Gemeinden wieder zusammenführen -> Immer gleiches Format für Datawrapper
+  if (nrow(results_notavailable) > 0) {
+    
+    results_notavailable$Ja_Stimmen_In_Prozent <- 0
+    results_notavailable$Nein_Stimmen_In_Prozent <- 0
+    results_notavailable$Gemeinde_color <- 50
+    
+    if (hist_check == TRUE) {
+      results_notavailable$Hist_Ja_Stimmen_In_Prozent <- NA
+      results_notavailable$Hist_Ja_Stimmen_Absolut <- NA
+      results_notavailable$Hist_Nein_Stimmen_In_Prozent <- NA
+      results_notavailable$Hist_Nein_Stimmen_Absolut <- NA
+    }
+    
+    results <- rbind(results,results_notavailable) %>%
+      arrange(Gemeinde_Nr)
+    
+  }
+  
+
+  #Texte speichern
+  #library(xlsx)
+  #write.xlsx(results,paste0(vorlagen_short[i],"_texte.xlsx"),row.names = FALSE)
+  
+  ###Output generieren für Datawrapper
+  
+  #Output Abstimmungen Gemeinde
+  output_dw <- get_output_gemeinden(results)
+  
+  #Output speichern
+  write.csv(output_dw,paste0("Output/",vorlagen_short[i],"_dw.csv"), na = "", row.names = FALSE, fileEncoding = "UTF-8")
+  
+  
+  ###Output generieren für Datawrapper Tessin
+  
+  #Output Abstimmungen Gemeinde
+  output_dw_ticino <- results[results$Kanton_Short == "TI",]
+  output_dw_ticino <- get_output_gemeinden(output_dw_ticino)
+
+  #Output speichern
+  write.csv(output_dw_ticino,paste0("Output/",vorlagen_short[i],"_dw_ticino.csv"), na = "", row.names = FALSE, fileEncoding = "UTF-8")
+  
+  ###Output generieren für Datawrapper Radiotop
+  
+  output_dw_radiotop <- results[results$Kanton_Short == "ZH" |
+                                  results$Kanton_Short == "SH" |
+                                  results$Kanton_Short == "TG" |
+                                  results$Kanton_Short == "SG" |
+                                  results$Kanton_Short == "AI" |
+                                  results$Kanton_Short == "AR",]
+  
+  output_dw_radiotop <- get_output_gemeinden(output_dw_radiotop)
+  
+  write.csv(output_dw_radiotop,paste0("Output/",vorlagen_short[i],"_dw_radiotop.csv"), na = "", row.names = FALSE, fileEncoding = "UTF-8")
+  
+  ###Output generieren für Datawrapper Schwyz Gebiete
+  
+  #Hoefe
+  output_dw_hoefe_SZ <- results[results$Gemeinde_Nr == 1323|
+                                  results$Gemeinde_Nr == 1322 |
+                                  results$Gemeinde_Nr == 1321,]
+  
+  output_dw_hoefe_SZ <- get_output_gemeinden(output_dw_hoefe_SZ)
+  
+  #March
+  output_dw_march_SZ <- results[results$Gemeinde_Nr > 1340 &
+                                  results$Gemeinde_Nr < 1350 ,]
+  
+  output_dw_march_SZ <- get_output_gemeinden(output_dw_march_SZ)
+
+  #Output speichern
+  write.csv(output_dw_hoefe_SZ,paste0("Output/",vorlagen_short[i],"_hoefe_SZ.csv"), na = "", row.names = FALSE, fileEncoding = "UTF-8")
+  write.csv(output_dw_march_SZ,paste0("Output/",vorlagen_short[i],"_march_SZ.csv"), na = "", row.names = FALSE, fileEncoding = "UTF-8")
+  
+  
+  ###Output generieren für Datawrapper Sarganserland
+  
+  #Hoefe
+  output_dw_sarganserland_SG <- results[results$Gemeinde_Nr > 3290 &
+                                          results$Gemeinde_Nr < 3300 ,]
+  
+  output_dw_sarganserland_SG <- get_output_gemeinden(output_dw_sarganserland_SG)
+  
+  write.csv(output_dw_sarganserland_SG,paste0("Output/",vorlagen_short[i],"_sarganserland_SG.csv"), na = "", row.names = FALSE, fileEncoding = "UTF-8")
+  
+  
+  #Log Kantone
+  cat(paste0("\n\n",Sys.time()," ",vorlagen_short[i],"\n"),file="Output/log_file.txt",append = TRUE)
+  
+  #Output Abstimmungen Kantone
+  output_dw_kantone <- get_output_kantone(results)
+  
+  write.csv(output_dw_kantone,paste0("Output/",vorlagen_short[i],"_dw_kantone.csv"), na = "", row.names = FALSE, fileEncoding = "UTF-8")
+  
+  cat(paste0("\nGenerated output for Vorlage ",vorlagen_short[i],"\n"))
+  
+  #Datawrapper-Karten aktualisieren
+  undertitel_de <- "Es sind noch keine Gemeinden ausgezählt."
+  undertitel_fr <- "Aucun résultat n'est encore connu."
+  undertitel_it <- "Nessun risultato è ancora noto."
+  
+  if (sum(results$Gebiet_Ausgezaehlt) > 0 ) {
+    
+    undertitel_de <- paste0("Es sind <b>",sum(results$Gebiet_Ausgezaehlt),"</b> von <b>",nrow(results),
+                            "</b> Gemeinden ausgezählt. Stand: <b>",
+                            round(results_national$jaStimmenInProzent,1)," %</b> Ja, <b>",
+                            round(100-results_national$jaStimmenInProzent,1)," %</b> Nein")
+    
+    undertitel_fr <- paste0("Le résultats de <b>",sum(results$Gebiet_Ausgezaehlt),"</b> des <b>",nrow(results),
+                            "</b> communes sont connus. Etat: <b>",
+                            round(results_national$jaStimmenInProzent,1)," %</b> oui, <b>",
+                            round(100-results_national$jaStimmenInProzent,1)," %</b> non")
+    
+    undertitel_it <- paste0("I risultati di <b>",sum(results$Gebiet_Ausgezaehlt),"</b> dei <b>",nrow(results),
+                            "</b> comuni sono noti. Stato: <b>",
+                            round(results_national$jaStimmenInProzent,1)," %</b> sì, <b>",
+                            round(100-results_national$jaStimmenInProzent,1)," %</b> no")
+  
+
+ 
+    #Karten Gemeinden
+    dw_edit_chart(datawrapper_codes[i,2],intro=undertitel_de,annotate=paste0("Letzte Aktualisierung: ",format(Sys.time(),"%d.%m.%Y %H:%M Uhr")))
+    dw_publish_chart(datawrapper_codes[i,2])
+    
+    dw_edit_chart(datawrapper_codes[i,4],intro=undertitel_fr,annotate=paste0("dernière mise à jour: ",format(Sys.time(),"%d.%m.%Y %H:%M Uhr")))
+    dw_publish_chart(datawrapper_codes[i,4])
+    
+    dw_edit_chart(datawrapper_codes[i,6],intro=undertitel_it,annotate=paste0("Ultimo aggiornamento: ",format(Sys.time(),"%d.%m.%Y %H:%M Uhr")))
+    dw_publish_chart(datawrapper_codes[i,6])
+    
+    #Karten Kantone
+    dw_edit_chart(datawrapper_codes[i,3],intro=undertitel_de,annotate=paste0("Letzte Aktualisierung: ",format(Sys.time(),"%d.%m.%Y %H:%M Uhr")))
+    dw_publish_chart(datawrapper_codes[i,3])
+    
+    dw_edit_chart(datawrapper_codes[i,5],intro=undertitel_fr,annotate=paste0("dernière mise à jour: ",format(Sys.time(),"%d.%m.%Y %H:%M Uhr")))
+    dw_publish_chart(datawrapper_codes[i,5])
+    
+    dw_edit_chart(datawrapper_codes[i,7],intro=undertitel_it,annotate=paste0("Ultimo aggiornamento: ",format(Sys.time(),"%d.%m.%Y %H:%M Uhr")))
+    dw_publish_chart(datawrapper_codes[i,7])
+    
+  }   
+
+}
